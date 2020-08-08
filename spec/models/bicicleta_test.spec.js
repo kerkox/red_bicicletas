c71e74c9505567d450f88475ff71ae9b97b1b75b
Paul Cortes <paulkerg@gmail.com>
2020-08-07 22:40:50 -0500

    metodos de test mejorados

--- spec/models/bicicleta_test.spec.js
+++ spec/models/bicicleta_test.spec.js
@@ -53,8 +53,32 @@ describe('Testing Bicicletas', function(){
           done();
         })
       })
-    })
-    
+    })    
+  })
+
+  describe('Bicicleta.findByCode', () => {
+    it('debe de devolver la bici con code 1', (done) => {
+      Bicicleta.allBicis(function(err, bicis){
+        expect(bicis.length).toBe(0);
+
+        var aBici = new Bicicleta({code: 1, color: "verde", modelo:"urbana"});
+        Bicicleta.add(aBici, function(err, newBici){
+          if (err) console.log(err);
+          var aBici2 = new Bicicleta({code:2, color: "roja", modelo:"deportiva"});
+          Bicicleta.add(aBici2, function(err, newBici){
+            if (err) console.log(err);
+            Bicicleta.findByCode(1, function(error, targetBici){
+              expect(targetBici.code).toBe(aBici.code);
+              expect(targetBici.color).toBe(aBici.color);
+              expect(targetBici.modelo).toBe(aBici.modelo);
+              done();
+            })
+          })
+        })
+
+      })
+      
+    })    
   })
   
   
