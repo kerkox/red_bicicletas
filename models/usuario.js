d3db1bdac88f38e84f662f351d733c301e0eb9fb
Paul Cortes <paulkerg@gmail.com>
2020-08-11 18:18:02 -0500

    implementacion de seguridad con bcrypt y actualizacion del modelo usuario para agregar los campos necesarios

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,54 @@
 var mongoose = require('mongoose');
 var Reserva = require('./reserva')
+const bcrypt = require('bcrypt')
+
+const saltRounds = 10;
+
 var Schema = mongoose.Schema;
 
+const validateEmail = function (email) {
+  const regExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+  return regExp.test(email);
+}
+
 var usuarioSchema = new Schema({
-  nombre: String
+  nombre: {
+    type: String,
+    trim: true,
+    required: [true, 'El nombre es obligatorio']
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, 'El email es obligatorio'],
+    lowercase: true,
+    validate: [validateEmail, 'Por favor ingrese un email valido'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/]
+  },
+  password: {
+    type: String,
+    required: [true, 'El password es obligatorio']
+  },
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date,
+  verificado: {
+    type: Boolean,
+    default: false
+  }
 })
+
+
+usuarioSchema.pre('save', function(next){
+  if ( this.isModified('password') ){
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+  }
+  next();
+})
+
+usuarioSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
 
 usuarioSchema.methods.reservar = function (biciId, desde, hasta, cb) {
   var reserva = new Reserva({
