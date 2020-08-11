be1e7e60dcdc1174c17eeb4f75ae6331274f4d8c
Paul Cortes <paulkerg@gmail.com>
2020-08-11 18:23:36 -0500

    Agregado de modelo Token

---
+++ models/Token.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const TokenSchema = new Schema({
+  _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Usuario'},
+  token: { type: String, required: true },
+  createdAt: { type: Date, required: true, default:  Date.now, expires: 43200 }
+});
+
+module.exports = mongoose.model('Token', TokenSchema)
