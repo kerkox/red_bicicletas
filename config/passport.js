2bd760ab62ef149a2e4f388829f1ec77caf80fc1
Paul Cortes <paulkerg@gmail.com>
2020-08-12 13:11:48 -0500

    implementacion de LocalStrategy en passport

--- config/passport.js
+++ config/passport.js
@@ -2,9 +2,17 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
 const Usuario = require('../models/usuario')
 
-passport.use(new LocalStrategy, {
+passport.use(new LocalStrategy(
+  function(email, password, done) {
+    Usuario.findOne({email: email}, function(err, usuarioDB) {
+      if(err) return done(err);
+      if( !usuarioDB) return done(null, false, {message: 'Email no existente o incorrecto'});
+      if(!usuarioDB.validaPassword(password)) return done(null, false, {message: 'Password Incorrecto'})
 
-});
+      return done(null, usuarioDB)
+    })
+  }
+));
 
 passport.serializeUser(function (user, cb) {
   cb(null, user.id);
