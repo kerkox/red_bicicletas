5c1076259fde200cfca04e447d2e6ef648847913
Paul Cortes <paulkerg@gmail.com>
2020-08-14 14:18:39 -0500

    Mejoras

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const passport = require('./config/passport');
+const Usuario = require('../models/usuario')
+const path = require('path')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
