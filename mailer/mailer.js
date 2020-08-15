94dd73e37d4d0dda8244faa7067c265fab4ac9fa
Paul Cortes <paulkerg@gmail.com>
2020-08-14 20:46:14 -0500

    Agregado de SendGrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,33 @@
 const nodemailer = require('nodemailer')
+const sgTransport = require('nodemailer-sendgrid-transport')
 
-const mailConfig = {
-  host: 'smtp.ethereal.email',
-  port: 587,
-  auth: {
-    user: 'lula31@ethereal.email',
-    pass: 'hJDv3VcHmjWryTEhW2'
-  }
+let mailConfig;
+
+if (process.env.NODE_ENV === "production") {
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET,
+        },
+    };
+    mailConfig = sgTransport(options);
+} else {
+    if (process.env.NODE_ENV === "staging") {
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET,
+            },
+        };
+        mailConfig = sgTransport(options);
+    } else {
+        mailConfig = {
+            host: "smtp.ethereal.email",
+            port: 587,
+            auth: {
+                user: process.env.ETHEREAL_EMAIL_USER_DEV,
+                pass: process.env.ETHEREAL_EMAIL_PASS_DEV
+            },
+        };
+    }
 }
 
 module.exports = nodemailer.createTransport(mailConfig);
