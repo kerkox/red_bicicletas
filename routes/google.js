fa37e4786eefd1f228d5a256bfa2fc83039339a6
Paul Cortes <paulkerg@gmail.com>
2020-08-15 00:24:03 -0500

    Mejoras Login Google

--- routes/google.js
+++ routes/google.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const passport = require('../config/passport');
 
 router.get(
-    "/",
-    passport.authenticate("google", {
-        scope: [
-            'https://www.googleapis.com/auth/plus.login',
-            'https://www.googleapis.com/auth/plus.profile.emails.read',
-            'profile',
-            'email'
-        ],
-    })
+  "/",
+  passport.authenticate("google", {
+    scope: [
+      'https://www.googleapis.com/auth/plus.login',
+      'https://www.googleapis.com/auth/plus.profile.emails.read',
+      'profile',
+      'email'
+    ],
+  })
 );
 
-router.get(
-    "/callback",
-    passport.authenticate("google", {
-        successRedirect: "/bicicletas",
-        failureRedirect: "/error,",
-    })
-);
+router.get('/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
+    res.redirect('/bicicletas');
+  });
 
 module.exports = router;
